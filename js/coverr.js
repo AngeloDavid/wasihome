f8cd10ecffcaa7c5da02e043ccb50c989e090c3e
AngeloDAvid <angelodavid95@hotmail.es>
2017-04-20 23:49:28 -0500

    cambios en el index

--- js/coverr.js
+++ js/coverr.js
@@ -61,3 +61,13 @@ function scaleBannerVideoSize(element){
 
     });
 }
+
+$(document).ready(function() {   
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+        });
